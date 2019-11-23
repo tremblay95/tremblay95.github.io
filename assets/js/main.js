5d6d535fd2b24cb4cc9a3813f6e1c0e48c54f9c1
Marc Tremblay <marc.tremblay1995@gmail.com>
2019-11-22 23:14:39 -0500

    everything seems to be ready to go... should only need to replace the placeholder projects with real projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,171 +1,83 @@
-/*
-	Eventually by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+---
+---
+var sc1 = {{ site.data.showcases.showcase1 | jsonify }};
+var sc2 = {{ site.data.showcases.showcase2 | jsonify }};
+var projects = {{ site.data.projects | jsonify }};
 
-(function() {
-
-	"use strict";
-
-	var	$body = document.querySelector('body');
-
-	// Methods/polyfills.
-
-		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
-
-		// canUse
-			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
-
-		// window.addEventListener
-			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
-
-	// Play initial animations on page load.
-		window.addEventListener('load', function() {
-			window.setTimeout(function() {
-				$body.classList.remove('is-preload');
-			}, 100);
-		});
-
-	// Slideshow Background.
-		(function() {
-
-			// Settings.
-				var settings = {
-
-					// Images (in the format of 'url': 'alignment').
-						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
-						},
-
-					// Delay.
-						delay: 6000
-
-				};
-
-			// Vars.
-				var	pos = 0, lastPos = 0,
-					$wrapper, $bgs = [], $bg,
-					k, v;
-
-			// Create BG wrapper, BGs.
-				$wrapper = document.createElement('div');
-					$wrapper.id = 'bg';
-					$body.appendChild($wrapper);
-
-				for (k in settings.images) {
-
-					// Create BG.
-						$bg = document.createElement('div');
-							$bg.style.backgroundImage = 'url("' + k + '")';
-							$bg.style.backgroundPosition = settings.images[k];
-							$wrapper.appendChild($bg);
-
-					// Add it to array.
-						$bgs.push($bg);
-
+$(document).ready(function(){
+	var $h = $(".header");
+	if($h.length){
+		var $nav = $(".nav-bg-dark");
+		if(!window.matchMedia('(max-width: 767px)').matches){
+			$(document).scroll(function() {
+				if($(this).scrollTop() + $(".nav-bg-dark").outerHeight(true) > $h.offset().top + $h.outerHeight(true)){
+					$nav.css("background", "{{site.sass_js_vars.dark}}").css("border-bottom", "2px solid {{site.sass_js_vars.highlight}}");
+				} else {
+					$nav.css("background", "transparent").css("border-bottom", "none");
 				}
+			});
+		} else {
+			$nav.css("background", "{{site.sass_js_vars.dark}}").css("border-bottom", "2px solid {{site.sass_js_vars.highlight}}");
+		}
+	}
 
-			// Main loop.
-				$bgs[pos].classList.add('visible');
-				$bgs[pos].classList.add('top');
+	$('.carousel').carousel('pause');
 
-				// Bail if we only have a single BG or the client doesn't support transitions.
-					if ($bgs.length == 1
-					||	!canUse('transition'))
-						return;
+	$('.navbar-nav>li>a').on('click', function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+});
 
-				window.setInterval(function() {
+function showProject(index){
+	var proj = projects[index];
+	fillProjectModal(proj);
+}
 
-					lastPos = pos;
-					pos++;
+function showShowcase(index){
+	var proj = index == 0 ? sc1 : sc2;
+	fillProjectModal(proj);
+}
 
-					// Wrap to beginning if necessary.
-						if (pos >= $bgs.length)
-							pos = 0;
+function fillProjectModal(proj){
 
-					// Swap top images.
-						$bgs[lastPos].classList.remove('top');
-						$bgs[pos].classList.add('visible');
-						$bgs[pos].classList.add('top');
+	var $link = $("#modal-link");
+	if(proj.link){
+		$link.removeClass("hidden");
+		$link.attr("href", proj.link);
+	}
+	else{
+		$link.addClass("hidden");
+	}
 
-					// Hide last image after a short delay.
-						window.setTimeout(function() {
-							$bgs[lastPos].classList.remove('visible');
-						}, settings.delay / 2);
+	$("#modal-title").text(proj.title);
+	$("#modal-desc").text(proj.description);
 
-				}, settings.delay);
+	var $gallery = $("#gallery");
+	var $inds = $("#indicators");
 
-		})();
+	$gallery.html(`
+	<div class="carousel-item active">
+		<img class="img-fluid" src="/assets/projects/`+ proj.title + `/` + proj.images[0] +`">
+	</div>
+	`);
+	
+	$inds.html(`<li data-target="#carousel" data-slide-to="0" class="active"></li>`);
 
-	// Signup Form.
-		(function() {
+	if(proj.images.length > 1){
+		$inds.removeClass("hidden");
+		$("#carousel>a").removeClass("hidden");
+	} else {
+		$inds.addClass("hidden");
+		$("#carousel>a").addClass("hidden");
+	}
 
-			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-					$message;
+	for(var i = 1; i < proj.images.length; ++i){
+		$gallery.append(`
+		<div class="carousel-item">
+			<img class="img-fluid" src="/assets/projects/`+ proj.title + `/` + proj.images[i] +`">
+		</div>
+		`);
+		$inds.append(`<li data-target="#carousel" data-slide-to="` + i + `"></li>`);
+	}
 
-			// Bail if addEventListener isn't supported.
-				if (!('addEventListener' in $form))
-					return;
-
-			// Message.
-				$message = document.createElement('span');
-					$message.classList.add('message');
-					$form.appendChild($message);
-
-				$message._show = function(type, text) {
-
-					$message.innerHTML = text;
-					$message.classList.add(type);
-					$message.classList.add('visible');
-
-					window.setTimeout(function() {
-						$message._hide();
-					}, 3000);
-
-				};
-
-				$message._hide = function() {
-					$message.classList.remove('visible');
-				};
-
-			// Events.
-			// Note: If you're *not* using AJAX, get rid of this event listener.
-				$form.addEventListener('submit', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Hide message.
-						$message._hide();
-
-					// Disable submit.
-						$submit.disabled = true;
-
-					// Process form.
-					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-					// but there's enough here to piece together a working AJAX submission call that does.
-						window.setTimeout(function() {
-
-							// Reset form.
-								$form.reset();
-
-							// Enable submit.
-								$submit.disabled = false;
-
-							// Show message.
-								$message._show('success', 'Thank you!');
-								//$message._show('failure', 'Something went wrong. Please try again.');
-
-						}, 750);
-
-				});
-
-		})();
-
-})();
+}
